e0e4bd9e1275db3c9914163bc996334c4086eca5
Shivin Devgon <shivin302@berkeley.edu>
2019-08-06 15:17:58 -0700

    Top bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,20 @@ import React from 'react'
 class Header extends React.Component {
     render() {
         return (
-            <section id="header">
-                <div className="inner">
-                    <span className="icon major fa-cloud"></span>
-                    <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
-                    little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
-                    <ul className="actions">
+            <section id="ac">
+                <div className="topnav">
+                    <a href="/" className="icon major fa-plane" style={{fontSize:'10px'}}></a>
+                    <a href="/" class = 'active'> PreFlyte </a>
+                    <a href="/" > About Us </a>
+                    <div class="topnav-right">
+                    <a href="/" > Get Funded </a>
+                    <a href="/" > Fund Someone </a>
+                    <a href="/" > Log In </a>
+                    <a href="/" > Sign Up </a>
+                    </div>
+                    {/* <ul className="actions">
                         <li><a href="#one" className="button scrolly">Discover</a></li>
-                    </ul>
+                    </ul> */}
                 </div>
             </section>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,37 @@ class Homepage extends React.Component {
         return (
             <Layout>
                 <Helmet title={siteTitle} />
+                {/* Trying to add carousel here */}
+                {/* <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <div class="carousel-inner">
+                        <div class="item active">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg/1920px-Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg" alt="Los Angeles" style={{width: '500px', height: '300px'}} />
+                        </div>
+
+                        <div class="item">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg/1920px-Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg" alt="Los Angeles" style={{width: '500px', height: '300px'}} />
+                        </div>
+
+                        <div class="item">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg/1920px-Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg" alt="Los Angeles" style={{width: '500px', height: '300px'}} />
+                        </div>
+                    </div>
+
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div> */}
 
                 <section id="one" className="main style1">
                     <div className="grid-wrapper">
